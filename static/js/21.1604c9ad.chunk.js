7bd964afbe05386ff99c660a6cba8a4d0ce3b2d8
Medard Mandane <medardm13@gmail.com>
2022-01-22 08:55:48 +0800

    Updates

---
+++ static/js/21.1604c9ad.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonppancake-frontend"]=this["webpackJsonppancake-frontend"]||[]).push([[21],{1171:function(n,a,e){"use strict";e.r(a);var t=e(972);a.default=t.Bar}}]);
+//# sourceMappingURL=21.1604c9ad.chunk.js.map
