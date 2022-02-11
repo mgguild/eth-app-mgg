2e826bf5a22dba0f18de821b8e738f62c7139358
Aldrick Bonaobra <git@aldrickb.com>
2022-02-11 22:55:33 +0800

    Revert "Remove USD and APR for temporary fix"
    This reverts commit 59c228d19b0521ce898ba996ff926314fcc345b7.

--- src/views/Farms/Farms.tsx
+++ src/views/Farms/Farms.tsx
@@ -422,14 +422,14 @@ const Farms: React.FC = () => {
              <Text fontSize='17px' bold color={theme.colors.MGG_accent2}>Total Tokens Staked</Text>
              <Text fontSize='20px'> {totalStaked} {mggFarm.lpSymbol}</Text>
            </Flex>
-           {/* <Flex flexDirection="column">
+           <Flex flexDirection="column">
              <Text fontSize='17px' bold color={theme.colors.MGG_accent2}>Total Value Locked</Text>
              <Text fontSize='20px'>- USD</Text>
            </Flex>
            <Flex flexDirection="column">
              <Text fontSize='17px' bold color={theme.colors.MGG_accent2}>APR</Text>
              <Text fontSize='20px'>{apr} % </Text>
-           </Flex> */}
+           </Flex>
         </InfoBox>
         </Flex>
           {/* <Flex style={isMobile ? {
